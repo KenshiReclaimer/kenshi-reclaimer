22774ccb7e6b134450cdd761898fd8d683639645
Greg Lando <kaos4d1@gmail.com>
2021-03-16 00:42:09 -0700

    idk

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,15 @@
 add_rules("mode.debug", "mode.release")
 
-target("Reclaimer")
+
+target("OgreMain")
+    set_kind("static")
+    add_links("$(builddir)/src/third-party/ogre/lib/OgreMain_x64.lib")
+    add_includedirs("$(builddir)/src/third-party/ogre/include", {public=true})
+    add_headerfiles("$(builddir)/src/third-party/ogre/include/**.h")
+target_end()
+
+target("Plugin_Reclaimer")
     set_kind("shared")
-    add_files("src/*.cpp")
+    add_files("src/**.cpp")
+    add_deps("OgreMain")
+target_end()
